2be7a15724054a8a5acfb096ada91ca4b6442d23
jonahrosenblum <jonaherosenblum@gmail.com>
2019-10-21 18:57:23 -0400

    just testing the simulation

--- run_simulation.py
+++ run_simulation.py
@@ -6,11 +6,24 @@ from population import mutate
 import itertools
 
 genotype_counter = itertools.count(0)
-population_size = 100
+population_size = 1000
 mutation_rate = .05
+maxf = 0
 
-pop = create_population(population_size, genotype_counter, nkclass)
-pop = get_fitnesses(pop)
-pop = reproduce(pop, population_size)
-mutate(pop, population_size, mutation_rate, genotype_counter)
+pop_map = dict()
+
+pop = create_population(population_size, genotype_counter, nkclass, pop_map)
+for i in range(1000):
+  pop = get_fitnesses(pop)
+    # average = 0
+    # for fit, freq in zip(pop.vs['fitness'], pop.vs['frequency']):
+    #   average += fit * freq
+    # print(average, max(pop.vs['fitness']))
+    #print(len(pop.vs['frequency']))
+  newmax = max(pop.vs['fitness'])
+  if (newmax > maxf):
+    maxf = newmax
+    print(maxf)
+  pop = reproduce(pop, population_size)
+  pop = mutate(pop, population_size, mutation_rate, genotype_counter, pop_map)
 #print(pop.vs['genotype'][0].fitness())
