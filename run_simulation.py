f086425d5d5208f6bc063de6689bc33c98b9a8fe
jonahrosenblum <jonaherosenblum@gmail.com>
2019-10-28 21:38:32 -0400

    measure num mutations happened when there is a new max fitness

--- run_simulation.py
+++ run_simulation.py
@@ -6,7 +6,7 @@ from population import mutate
 import itertools
 
 genotype_counter = itertools.count(0)
-population_size = 1000
+population_size = 10000
 mutation_rate = .05
 maxf = 0
 
@@ -15,15 +15,20 @@ pop_map = dict()
 pop = create_population(population_size, genotype_counter, nkclass, pop_map)
 for i in range(1000):
   pop = get_fitnesses(pop)
+  # if i % 100 == 0:
+  #     print(pop.vs['num_mutations'])
+    # print(pop.vs['frequency'])
     # average = 0
     # for fit, freq in zip(pop.vs['fitness'], pop.vs['frequency']):
     #   average += fit * freq
-    # print(average, max(pop.vs['fitness']))
+
+    # print(average)
+    #print(average, max(pop.vs['fitness']))
     #print(len(pop.vs['frequency']))
   newmax = max(pop.vs['fitness'])
   if (newmax > maxf):
     maxf = newmax
-    print(maxf)
+    print(pop.vs.find(fitness=maxf)['num_mutations'])
   pop = reproduce(pop, population_size)
   pop = mutate(pop, population_size, mutation_rate, genotype_counter, pop_map)
 #print(pop.vs['genotype'][0].fitness())
