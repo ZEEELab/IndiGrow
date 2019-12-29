a75962a79e270022380995e86ffd054357c7443e
jonahrosenblum <jonaherosenblum@gmail.com>
2019-12-29 15:42:26 -0500

    make timestep increment part of timestep function, no need to pass in argument anymore

--- run_simulation.py
+++ run_simulation.py
@@ -1,18 +1,20 @@
 from user_classes.infectionclass import infectionclass
+from user_classes.nk_fuji_class import nk_fuji_class
 from user_environments.infectionenvironment import infectionenvironment
+from user_environments.nk_fuji_environment import nk_fuji_environment
 from IndiGrow import IndiGrow
 import itertools
 import random
 import numpy
 
 random.seed(13221)
-numpy.random.seed(12137)
+numpy.random.seed(12907)
 
 # genotype_counter = itertools.count(0)
 population_size = 10E7
 mutation_rate = 10E-4
 
-test = IndiGrow(population_size, mutation_rate, genotype=infectionclass, environment=infectionenvironment)
-for i in range(500):
-    test.timestep(i)
+test = IndiGrow(population_size, mutation_rate, genotype=nk_fuji_class, environment=nk_fuji_environment)
+for i in range(10000):
+    test.timestep()
    
