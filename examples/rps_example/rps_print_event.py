5a39f6f4980f672d2c68103b51a5101201b92dc1
jonahrosenblum <jonaherosenblum@gmail.com>
2020-06-09 23:10:41 -0400

    update rps_simulation

--- examples/rps_example/rps_print_event.py
+++ examples/rps_example/rps_print_event.py
@@ -1,10 +1,15 @@
+import json
+
 class rps_print_event:
     def __init__(self):
         self.__IndiGrow__ = None
+        # clear current contents of file
+        open('rps_data.txt', 'w').close()
 
     def __run_handler__(self):
         frequencies = {}
         for i in self.__IndiGrow__.population.vs:
             frequencies[i['genotype'].type] = i['frequency']
-        print(frequencies)
+        with open('rps_data.txt', 'a+') as write_file:
+            write_file.write(json.dumps(frequencies) + '\n')
         
--- examples/rps_example/rps_simulation.py
+++ examples/rps_example/rps_simulation.py
@@ -6,18 +6,18 @@ import numpy
 
 
 if __name__ == '__main__':
-    random.seed(182)
-    numpy.random.seed(1277)
+    random.seed(1892)
+    numpy.random.seed(17)
 
-    population_size = 10E7
-    mutation_rate = 10E-4
+    population_size = 1E5
+    num_generations = 1000
 
-    test = IndiGrow(population_size=10E7, mutation_rate=0)
+    rps_simulation = IndiGrow(population_size=population_size, mutation_rate=0)
 
-    test.add_genotype(genotype=rps_class('rock'), frequency=98/100)
-    test.add_genotype(genotype=rps_class('paper'), frequency=1/100)
-    test.add_genotype(genotype=rps_class('scissors'), frequency=1/100)
-    test.add_event(event_class=rps_print_event, trigger_every=100)
+    rps_simulation.add_genotype(genotype=rps_class('rock'), frequency=50/100)
+    rps_simulation.add_genotype(genotype=rps_class('paper'), frequency=25/100)
+    rps_simulation.add_genotype(genotype=rps_class('scissors'), frequency=25/100)
+    rps_simulation.add_event(event_class=rps_print_event, trigger_every=10)
 
-    for i in range(300):
-        test.timestep()
+    for i in range(num_generations):
+        rps_simulation.timestep()
